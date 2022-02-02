18599c39fd48a33289e13a3448e6b744835beaec
saknet <andreas.andra@helsinki.fi>
2022-02-02 11:27:24 +0200

    added obsfeature collection

--- sql/fill_tables.sql
+++ sql/fill_tables.sql
@@ -3,3 +3,6 @@ VALUES('Viikki Environmental House', 'high-performance office building in Helsin
 
 INSERT INTO featureofinterest (name, description, encodingtype, feature) 
 VALUES('Korkeasaari Zoo', 'Island Zoo in Helsinki', 'application/vnd.geo+json', ST_GeomFromGeoJSON('{ "type": "Point", "coordinates": [24.990982, 60.181069] }') );
+
+INSERT INTO observedfeature (feature) 
+VALUES(ST_GeomFromGeoJSON('{ "type": "Polygon", "coordinates": [[[24.956651, 60.194194], [24.957127, 60.194216], [24.957423, 60.194545], [24.957109, 60.194715], [24.956385, 60.194579], [24.956330, 60.194343]]] }') );
